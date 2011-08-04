b3b09c6cf5f675fba2c00a71398c3e2d785a09fe
Neal Pisenti <npisenti@gmail.com>
2011-08-04 08:34:33 -0700

    snapshot, before starting to work in earnest

--- blog.py
+++ blog.py
@@ -3,18 +3,30 @@ import base64
 import os
 
 urls = (
-  '/', 'blog',
+  '/', 'home',
+  '/index', 'index',
   '/(.*)', 'post')
 
 app = web.application(urls, globals(), autoreload=True)
 
 render = web.template.render('templates/', base='base')
+render_partial = web.template.render('templates/')
 
-class blog:
+class index:
     def GET(self):
-        content = map(lambda x: x[:-5], os.listdir('pages'))
-        return render.blog("Templates demo", "Hello", 'posts:', content)
+        posts = map(lambda x: x[:-5], os.listdir('pages'))
+        return render.index("WebPyBlog", posts)
 
+class home:
+    def GET(self):
+        post_list = os.listdir('pages')
+        posts = []
+        for post in post_list:
+            f = open('pages/' + post, 'rb')
+            posts += [render_partial.post(post[:-5], f.read())]
+            f.close()
+            
+        return render.blog("WebPyBlog", posts)
 
 class post:
     def GET(self, url):
@@ -25,9 +37,11 @@ class post:
         except IOError:
             content = "This page does not exist!"
         
-        #content = base64.b64decode(web.websafe(url))
-        #content = os.getcwd()
-        return render.post(web.websafe(url), web.websafe(url), content) 
-        
+        return render.post(web.websafe(url), content)
+
+
 if __name__ == "__main__":
     app.run()
+    
+    
+    
